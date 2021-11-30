35f81fafb382e4708da4b70892ab30036a26e41d
AurelHolotnak <aholotnak@gmail.com>
2021-11-30 19:11:34 +0100

    Info

--- city4people/src/Info.js
+++ city4people/src/Info.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import InfoCard from "./InfoCard";
-export default function InfoScreen(){
+export default function Info(){
 
     return(
         <ScrollView >
