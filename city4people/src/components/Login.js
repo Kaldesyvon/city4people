29b6edd1ecb983de16b37d3558b6b76efd7e7e7a
Peter Poprík <peter.poprik@student.tuke.sk>
2021-11-30 22:00:16 +0100

    petko: sign in a login

--- city4people/src/components/Login.js
+++ city4people/src/components/Login.js
@@ -1,47 +1,70 @@
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 import React from 'react';
 import {Link} from 'react-router-native';
+import {Divider} from 'react-native-elements';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 
 const Login = () => {
     return (
         <View style={{flex: 1, flexDirection: 'column'}}>
-            <View style={{flex: 1}}>
-                <Text>Login</Text>
+            <View style={{flex: 1, alignItems: 'center'}}>
+                <Text style={[styles.title, {paddingTop: 50}]}>Login</Text>
             </View>
-            <View style={{flex: 2}}>
-                <Text>WIP</Text>
+            <View style={{flex: 2, alignItems: 'center'}}>
+                <View style={{width: 320}}>
+                    <View style={[styles.inputBox, {marginBottom: 45, borderColor: '#1faa00'}]}>
+                        <Text style={styles.label}>Nickname</Text>
+                        <TextInput style={styles.inputBoxText} maxLength={20} />
+                    </View>
+                    <View style={styles.inputBox}>
+                        <Text style={styles.label}>Password</Text>
+                        <TextInput style={styles.inputBoxText} secureTextEntry={true} maxLength={20} />
+                    </View>
+                </View>
             </View>
             <View style={{flex: 1}}>
-                <TouchableOpacity style={styles.button}>
-                    {/*<Text*/}
-                    {/*    style={{*/}
-                    {/*        color: 'white',*/}
-                    {/*        fontSize: 20,*/}
-                    {/*        fontWeight: 'bold',*/}
-                    {/*    }}>*/}
-                    {/*    Login*/}
-                    {/*</Text>*/}
-                    <Link to={"/Navigation"}>
-                        <Text> awdawd</Text>
-                    </Link>
-                </TouchableOpacity>
+                <Link style={styles.button} to={'/Navigation'}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </Link>
+                <Divider style={{marginTop: 45, width: 250, alignSelf: 'center'}} width={2}/>
+                <View style={{flexDirection: 'row', marginTop: 16, width: 250, alignSelf: 'center'}}>
+                    <Text style={[styles.label, {color: '#9A9A9A'}]}>
+                        Do not have a City4People account?
+                    </Text>
+                    <Pressable>
+                        <Text style={[styles.label, {color: '#1faa00'}]}>  Register</Text>
+                    </Pressable>
+                </View>
             </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    title: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: '#1faa00',
+    },
     text: {
         color: 'black',
     },
-    mapContainer: {
-        height: 400,
-        width: 400,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
+    label: {
+        fontSize: 12,
+        fontWeight: 'bold',
+        color: 'black',
+        paddingTop: 3,
     },
-    map: {
-        ...StyleSheet.absoluteFillObject,
+    inputBoxText: {
+        fontSize: 16,
+        paddingTop: 5,
+    },
+    inputBox: {
+        height: 60,
+        paddingLeft: 8,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: 'black',
     },
     logo: {
         marginTop: 100,
--- city4people/src/components/Signin.js
+++ city4people/src/components/Signin.js
@@ -1,10 +1,10 @@
 import {Image, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View} from "react-native";
-import React from "react";
-import {Link} from "react-router-native";
+import React from 'react';
+import {Link} from 'react-router-native';
 
 const Signin = () => {
     return (
-        <View style={{flex: 1, flexDirection: 'column'}}>
+        <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'white'}}>
             <View style={{flex: 2}}>
                 <Image
                     style={styles.logo}
@@ -12,14 +12,8 @@ const Signin = () => {
                 />
             </View>
             <View style={{flex: 2}}>
-                {/*<TouchableOpacity style={styles.button}>*/}
-                {/*    /!*<Text style={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>Login</Text>*!/*/}
-                {/*    <Link to={"/Login"}>*/}
-                {/*        <Text> awdawd</Text>*/}
-                {/*    </Link>*/}
-                {/*</TouchableOpacity>*/}
                 <Link style={styles.button} to={'/Login'}>
-                    <Text>LOGIN</Text>
+                    <Text style={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>Login</Text>
                 </Link>
 
                 <TouchableOpacity style={styles.button}>
@@ -36,29 +30,18 @@ const styles = StyleSheet.create({
     text: {
         color: 'black',
     },
-    mapContainer: {
-        height: 400,
-        width: 400,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-    },
-    map: {
-        ...StyleSheet.absoluteFillObject,
-    },
     logo: {
         marginTop: 100,
-        // paddingTop: 150,
         alignSelf: 'center',
-        width: 170,
-        height: 170,
+        width: 180,
+        height: 180,
     },
     button: {
         alignItems: 'center',
         width: 250,
         height: 55,
         alignSelf: 'center',
-        marginTop: 35,
-        // padding: 15,
+        marginTop: 40,
         paddingTop: 12,
         backgroundColor: '#1faa00',
         borderRadius: 50,
