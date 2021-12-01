446a27f233b3937346824b8192a657ff03f34e34
mm052if <marek.motuz@student.tuke.sk>
2021-12-01 23:02:04 +0100

    Account: adding scroll

--- city4people/src/components/Profile.js
+++ city4people/src/components/Profile.js
@@ -21,44 +21,50 @@ export default function Profile(){
                 </Text>
             </View>
 
-            <View style = {Styles.imageCircle}>
-                <Image source={require('../assets/images/man-modified.png')} style={Styles.profile_image}/>
-            </View>
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                style={{
+                    marginBottom: 75,
+                }}>
+                <View style = {Styles.imageCircle}>
+                    <Image source={require('../assets/images/man-modified.png')} style={Styles.profile_image}/>
+                </View>
 
-            <View
-                style={Styles.buttonContainerWhite}>
-                <Text style={Styles.Button_box_white}>Janko Hrasko</Text>
-                <Text style={Styles.rep_box}>Reputation: 2</Text>
-            </View>
+                <View
+                    style={Styles.buttonContainerWhite}>
+                    <Text style={Styles.Button_box_white}>Janko Hrasko</Text>
+                    <Text style={Styles.rep_box}>Reputation: 2</Text>
+                </View>
 
-            <Link 
-                style={Styles.buttonContainerGreen}
-                to={'/upvotes'}>
-                <Text style={Styles.Button_box_green}>Upvotes</Text>
-            </Link>
+                <Link 
+                    style={Styles.buttonContainerGreen}
+                    to={'/upvotes'}>
+                    <Text style={Styles.Button_box_green}>Upvotes</Text>
+                </Link>
 
-            <SelectDropdown
-                defaultButtonText = 'Language'
-                buttonStyle = {Styles.dropDownContainerGreen}
-                buttonTextStyle = {Styles.Button_box_green}
-                dropdownStyle = {Styles.Button_box_green}
-                // rowStyle = {Styles.Button_box_green}
-                rowTextStyle = {Styles.Button_box_green}
-                data={countries}
-                onSelect={(selectedItem, index) => {}}
-                buttonTextAfterSelection={(selectedItem, index) => {
-                    return selectedItem
-                }}
-                rowTextForSelection={(item, index) => {
-                    return item
-                }}
+                <SelectDropdown
+                    defaultButtonText = 'Language'
+                    buttonStyle = {Styles.dropDownContainerGreen}
+                    buttonTextStyle = {Styles.Button_box_green}
+                    dropdownStyle = {Styles.Button_box_green}
+                    // rowStyle = {Styles.Button_box_green}
+                    rowTextStyle = {Styles.Button_box_green}
+                    data={countries}
+                    onSelect={(selectedItem, index) => {}}
+                    buttonTextAfterSelection={(selectedItem, index) => {
+                        return selectedItem
+                    }}
+                    rowTextForSelection={(item, index) => {
+                        return item
+                    }}
 
-            />
+                />
 
-            <TouchableOpacity
-                style={Styles.buttonContainerGreen}>
-                <Text style={Styles.Button_box_green}>About</Text>
-            </TouchableOpacity>
+                <TouchableOpacity
+                    style={Styles.buttonContainerGreen}>
+                    <Text style={Styles.Button_box_green}>About</Text>
+                </TouchableOpacity>
+            </ScrollView>
 
             <Navigation
                 activeTab='profile'>
