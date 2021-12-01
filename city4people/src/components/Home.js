a401cba4fde5bb430fdeeecef6f18fd34839107b
Oliver Novotný <oliver.novotny@student.tuke.sk>
2021-12-01 11:04:32 +0100

    Added bottom button

--- city4people/src/components/Home.js
+++ city4people/src/components/Home.js
@@ -1,12 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {
-    StyleSheet,
-    View,
-    Image,
-    Text,
-    ScrollView,
-    TouchableOpacity,
-} from 'react-native';
+import {View, Image, Text, ScrollView, TouchableOpacity} from 'react-native';
 import getAxios from '../api/getAxios';
 import Icon from 'react-native-ionicons';
 import {Styles} from '../Styles';
@@ -25,87 +18,107 @@ const Home = () => {
     }, []);
 
     return (
-        <ScrollView
-            alignItems="center"
-            showsVerticalScrollIndicator={false}
-            style={{
-                backgroundColor: '#EEEEEE',
-                display: 'flex',
-                flex: 0,
-                marginBottom: 75,
-            }}>
-            {taskList.map(task => {
-                return (
-                    <View key={task.id} style={Styles.taskCard}>
-                        <Image
-                            resizeMode="cover"
-                            style={Styles.taskImage}
-                            source={{
-                                uri: task.photo,
-                            }}
-                        />
+        <>
+            <TouchableOpacity
+                style={{
+                    borderWidth: 4,
+                    backgroundColor: 'white',
+                    borderColor: '#1faa00',
+                    position: 'absolute',
+                    width: 70,
+                    height: 70,
+                    bottom: 110,
+                    right: 30,
+                    zIndex: 5,
+                    borderRadius: 50,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                <Icon name="add" style={{color: '#1faa00'}} size={50} />
+            </TouchableOpacity>
+            <ScrollView
+                alignItems="center"
+                showsVerticalScrollIndicator={false}
+                style={{
+                    backgroundColor: '#EEEEEE',
+                    display: 'flex',
+                    flex: 0,
+                    marginBottom: 75,
+                }}>
+                {taskList.map(task => {
+                    return (
+                        <View key={task.id} style={Styles.taskCard}>
+                            <Image
+                                resizeMode="cover"
+                                style={Styles.taskImage}
+                                source={{
+                                    uri: task.photo,
+                                }}
+                            />
 
-                        <View style={Styles.textContainer}>
-                            <Text style={{fontSize: 12}}>
-                                Published {task.published}
-                            </Text>
-                            <Text style={Styles.title}>{task.title}</Text>
-                            <View
-                                style={{
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                    marginTop: 8,
-                                }}>
-                                <View
-                                    style={{
-                                        width: '50%',
-                                        justifyContent: 'center',
-                                    }}>
-                                    <Text
-                                        style={{
-                                            color: 'black',
-                                            fontSize: 16,
-                                        }}>
-                                        Priority: {'!'.repeat(task.priority)}
-                                    </Text>
-                                </View>
+                            <View style={Styles.textContainer}>
+                                <Text style={{fontSize: 12}}>
+                                    Published {task.published}
+                                </Text>
+                                <Text style={Styles.title}>{task.title}</Text>
                                 <View
                                     style={{
                                         display: 'flex',
                                         flexDirection: 'row',
-                                        alignItems: 'center',
+                                        marginTop: 8,
                                     }}>
-                                    <Icon name="pin" color="black" />
-                                    <Text
+                                    <View
                                         style={{
-                                            color: 'black',
-                                            fontSize: 16,
-                                            paddingLeft: 8,
+                                            width: '50%',
+                                            justifyContent: 'center',
                                         }}>
-                                        {task.location}
-                                    </Text>
+                                        <Text
+                                            style={{
+                                                color: 'black',
+                                                fontSize: 16,
+                                            }}>
+                                            Priority:{' '}
+                                            {'!'.repeat(task.priority)}
+                                        </Text>
+                                    </View>
+                                    <View
+                                        style={{
+                                            display: 'flex',
+                                            flexDirection: 'row',
+                                            alignItems: 'center',
+                                        }}>
+                                        <Icon name="pin" color="black" />
+                                        <Text
+                                            style={{
+                                                color: 'black',
+                                                fontSize: 16,
+                                                paddingLeft: 8,
+                                            }}>
+                                            {task.location}
+                                        </Text>
+                                    </View>
                                 </View>
+                                <TouchableOpacity style={Styles.outerbutton}>
+                                    <View style={Styles.innerButton}>
+                                        <Text
+                                            style={{
+                                                fontSize: 16,
+                                                color: 'white',
+                                            }}>
+                                            Detail
+                                        </Text>
+                                        <Icon
+                                            name="arrow-forward"
+                                            size={20}
+                                            color="white"></Icon>
+                                    </View>
+                                </TouchableOpacity>
                             </View>
-                            <TouchableOpacity style={Styles.outerbutton}>
-                                <View style={Styles.innerButton}>
-                                    <Text
-                                        style={{
-                                            fontSize: 16,
-                                            color: 'white',
-                                        }}>
-                                        Detail
-                                    </Text>
-                                    <Icon
-                                        name="arrow-forward"
-                                        size={20}
-                                        color="white"></Icon>
-                                </View>
-                            </TouchableOpacity>
                         </View>
-                    </View>
-                );
-            })}
-        </ScrollView>
+                    );
+                })}
+            </ScrollView>
+        </>
     );
 };
 
